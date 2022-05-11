452d6c461a25c4b3caa21d701039973c1b652d15
Robert Di Ninno <robertdininno@gmail.com>
2022-05-11 17:01:13 +0000

    add standard input function, and handlesuerinput function

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const {connect} = require('./client');
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
+
+
 console.log("Connecting ...");
 connect();
