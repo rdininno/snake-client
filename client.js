066af8b6712f4d9a0c51f6c12c9c5d95deeff64d
Robert Di Ninno <robertdininno@gmail.com>
2022-05-11 16:42:24 +0000

    client side file

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here,
+    port: '50541' // PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: rjd');
+  });
+
+  conn.on("data", (data) => {
+    console.log(data)
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect};
