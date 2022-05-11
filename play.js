a80895a5fb9a3bb57d094f0783e10493f38fc1f0
Robert Di Ninno <robertdininno@gmail.com>
2022-05-11 17:06:14 +0000

    removed input functions to own file

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const {connect} = require('./client');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
-
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
